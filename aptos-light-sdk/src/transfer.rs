eac75842a0794c65bf15273f51163fe89a6dcdda
0xhappyBoy <aamyj@vip.qq.com>
2024-07-04 05:23:39 +0800

    update : update send_txn_hash function

--- aptos-light-sdk/src/transfer.rs
+++ aptos-light-sdk/src/transfer.rs
@@ -9,7 +9,7 @@ use aptos_sdk::{
 
 use crate::{client::AptosClient, utils::wrap_coin_amount};
 
-/// create a vanity account
+/// create a txn hash
 ///
 /// # Examples
 ///
@@ -28,5 +28,23 @@ pub async fn create_txn_hash(
     let txn_hash: Result<aptos_sdk::rest_client::PendingTransaction, anyhow::Error> = coin_client
         .transfer(from, to.address(), wrap_coin_amount(amount), None)
         .await;
-    Err("123".to_string())
+    Err("err".to_string())
+}
+
+/// send a txn hash
+///
+/// # Examples
+///
+/// ```
+/// let aptos_client = AptosClient::new(Mode::DEV);
+/// let txn = crate_txn_hash(aptos_client,from address,to address,amount)?
+/// send_txn_hash(aptos_client,txn)
+/// ```
+pub async fn send_txn_hash(
+    aptos_client: &mut AptosClient,
+    txn_hash: PendingTransaction,
+) -> Result<PendingTransaction, String> {
+    let rest_client: Client = aptos_client.rest_client().clone().unwrap();
+    rest_client.wait_for_transaction(&txn_hash).await;
+    Err("err".to_string())
 }
